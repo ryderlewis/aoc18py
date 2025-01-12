e68bbc7e2b5c126407920c9cc4ce622c9f3c04ac
Ryder Lewis <ryder.lewis@gmail.com>
2025-01-12 13:59:30 -0800

    day 2

--- days/day02.py
+++ days/day02.py
@@ -1,4 +1,5 @@
 from .day import Day
+from collections import Counter
 
 
 class Day02(Day):
@@ -6,7 +7,25 @@ class Day02(Day):
         super().__init__(*args, **kwargs)
 
     def part1(self) -> str:
-        return "dayXX 1"
+        boxes = self.parse()
+        two_count = 0
+        three_count = 0
+        for b in boxes:
+            c = Counter(b)
+            if 2 in c.values():
+                two_count += 1
+            if 3 in c.values():
+                three_count += 1
+        return str(two_count*three_count)
 
     def part2(self) -> str:
-        return "dayXX 2"
+        boxes = self.parse()
+        for i, b1 in enumerate(boxes):
+            for b2 in boxes[i+1:]:
+                diff_count = sum(1 for c1, c2 in zip(b1, b2) if c1 != c2)
+                if diff_count == 1:
+                    return ''.join(c1 for c1, c2 in zip(b1, b2) if c1 == c2)
+        return ""
+
+    def parse(self) -> list[str]:
+        return self.data_lines()
