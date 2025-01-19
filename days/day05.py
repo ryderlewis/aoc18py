85a576fd57d16b26869acb8d51a1894c7882fc48
Ryder Lewis <ryder.lewis@gmail.com>
2025-01-18 23:52:35 -0800

    day 5

--- days/day05.py
+++ days/day05.py
@@ -1,3 +1,5 @@
+from typing import Iterable
+
 from .day import Day
 
 
@@ -5,8 +7,26 @@ class Day05(Day):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    @staticmethod
+    def reacted(polymer: Iterable[str]) -> int:
+        new = []
+        for c in polymer:
+            if len(new) == 0:
+                new.append(c)
+                continue
+            if new[-1].lower() == c.lower() and new[-1] != c:
+                new.pop()
+            else:
+                new.append(c)
+        return len(new)
+
     def part1(self) -> str:
-        return "dayXX 1"
+        polymer = self.data_lines()[0]
+        return str(self.reacted(polymer))
 
     def part2(self) -> str:
-        return "dayXX 2"
+        polymer = self.data_lines()[0]
+        mlen = self.reacted(polymer)
+        for o in range(ord('a'), ord('z')+1):
+            mlen = min(mlen, self.reacted((p for p in polymer if p.lower() != chr(o))))
+        return str(mlen)
